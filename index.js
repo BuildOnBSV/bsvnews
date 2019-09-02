4da4deb5ddf506706eb74ebfd69f3f42ec29e4bd
Andrew Kondelin <akondelin@Andrews-MacBook-Pro.local>
2019-09-02 08:01:50 +0800

    initial files

---
+++ index.js
@@ -0,0 +1,62 @@
+const {
+  planaria
+} = require("neonplanaria")
+const MongoClient = require('mongodb')
+var db
+const connect = (cb) => {
+  MongoClient.connect("mongodb://localhost:27017", {
+    useNewUrlParser: true
+  }, (err, client) => {
+    if (err) {
+      console.log("retrying...")
+      setTimeout(() => {
+        connect(cb)
+      }, 1000)
+    } else {
+      db = client.db("weather")
+      cb()
+    }
+  })
+}
+planaria.start({
+  filter: {
+    "from": 586000,
+    "q": {
+      //"find": { "in.e.a": "3FgFhED2mX2MgdZ5dhUrjn4V1Vu5RKNbp8" },
+      "find": {
+        "out.s2": "bsvnewsio1"
+      },
+      "project": {
+        "out.s3": 1,
+        "out.s4": 1,
+        "out.s5":1,
+        "out.s6":1,
+        "out.s7":1
+      }
+    }
+  },
+  onmempool: async (e) => {
+    await db.collection("a").insertMany([e.tx])
+  },
+  onblock: async (e) => {
+    await db.collection("a").insertMany(e.tx)
+  },
+  onstart: (e) => {
+    return new Promise(async (resolve, reject) => {
+      if (!e.tape.self.start) {
+        await planaria.exec("docker", ["pull", "mongo:4.0.4"])
+        await planaria.exec("docker", ["run", "-d", "-p", "27017-27019:27017-27019", "--name", "mongo", "-v", process.cwd() + "/db:/data/db", "mongo:4.0.4"])
+        await planaria.exec("docker", ["build", "-t", "app", "app/."])
+        await planaria.exec("docker", ["run", "-d", "-p", "4000:4000", "--link", "mongo:mongo","--name", "app", "app"])
+      }
+      connect(() => {
+        if (e.tape.self.start) db.collection("c").deleteMany({
+          "blk.i": {
+            "$gt": e.tape.self.end
+          }
+        }).then(resolve)
+        else resolve()
+      })
+    })
+  }
+})
