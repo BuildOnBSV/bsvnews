0ec163a97755aefe3d90efd45cbe2d54a64fac4b
Andrew Kondelin <akondelin@Andrews-MacBook-Pro.local>
2019-09-03 16:54:44 +0800

    update

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,11 +5,6 @@ sudo yum install -y gcc-c++ make git docker nodejs
 sudo service docker start
 echo 'Woot1!' > /home/ec2-user/user-script-output1.txt
 cd /home/ec2-user
-sudo docker stop mongo || true
-sudo docker rm mongo || true
-sudo docker stop app || true
-sudo docker rm app || true
-sudo rm tape.txt || true
 sudo node index &>/dev/null &
 sudo node server &>/dev/null &
 echo 'Woot2!' > /home/ec2-user/user-script-output2.txt
