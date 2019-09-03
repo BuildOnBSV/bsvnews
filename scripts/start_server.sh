507f5f48c0b22e34f2b96c3dcb1aca5189491bde
Andrew Kondelin <akondelin@Andrews-MacBook-Pro.local>
2019-09-03 16:37:21 +0800

    improved try syntax

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,11 +5,11 @@ sudo yum install -y gcc-c++ make git docker nodejs
 sudo service docker start
 echo 'Woot1!' > /home/ec2-user/user-script-output1.txt
 cd /home/ec2-user
-sudo docker stop mongo ||
-sudo docker rm mongo ||
-sudo docker stop app ||
-sudo docker rm app ||
-sudo rm tape.txt ||
+{ sudo docker stop mongo } || { }
+{ sudo docker rm mongo } || { }
+{ sudo docker stop app } || { }
+{ sudo docker rm app } || { }
+{ sudo rm tape.txt } || { }
 sudo node index &>/dev/null &
 sudo node server &>/dev/null &
 echo 'Woot2!' > /home/ec2-user/user-script-output2.txt
