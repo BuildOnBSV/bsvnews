ba0a9dcedb674fb05659923c9d2c5201850c6b4c
Andrew Kondelin <akondelin@Andrews-MacBook-Pro.local>
2019-09-03 17:15:28 +0800

    cleanup

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -x
-echo 'Woot!' > /home/ec2-user/user-script-output0.txt
 curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
 sudo yum install -y gcc-c++ make git docker nodejs
 sudo service docker start
-echo 'Woot1!' > /home/ec2-user/user-script-output1.txt
 cd /home/ec2-user
 sudo docker stop mongo
 sudo docker rm mongo
@@ -12,4 +10,3 @@ sudo docker rm app
 sudo rm tape.txt
 sudo node index &>/dev/null &
 sudo node server &>/dev/null &
-echo 'Woot2!' > /home/ec2-user/user-script-output2.txt
